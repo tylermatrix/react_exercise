986537e1afa50032cd00734a99c9742a9c3bbb8c
Tyler Matrix <tylermatrix0@gmail.com>
2021-05-26 19:19:30 -0400

    reset app for second exercise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Todo from "./components/Todo";
-import Modal from "./components/Modal";
-import Backdrop from "./components/Backdrop";
-
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>My Todos</h1>
-        <Todo title="Learn React" />
-        <Todo title="Learn Next" />
-        <Todo title="Build a project" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
